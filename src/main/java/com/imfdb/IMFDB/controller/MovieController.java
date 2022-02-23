4e785ee5d9baa46ac0e429e5876e26a75c57b955
danielhogstrom <daniel.hogstrom@gmail.com>
2022-02-23 16:25:38 +0100

    add a lot

---
+++ src/main/java/com/imfdb/IMFDB/controller/MovieController.java
@@ -0,0 +1,22 @@
+package com.imfdb.IMFDB.controller;
+
+import com.imfdb.IMFDB.entity.Movie;
+import com.imfdb.IMFDB.repository.MovieRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+@SuppressWarnings("unused")
+@Controller
+public class MovieController {
+
+    MovieRepository repository = new MovieRepository();
+
+    @GetMapping("/")
+    public String getIndex(Model model){
+        List<Movie> movies = repository.getMovies();
+        model.addAttribute("movies", movies);
+        return "index";
+    }
+}
---
+++ src/main/java/com/imfdb/IMFDB/entity/Movie.java
@@ -0,0 +1,20 @@
+package com.imfdb.IMFDB.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Movie {
+    private String title;
+    private String genre;
+    private int yearMade;
+    private String director;
+    private int recommendedAge;
+    private String imgUrl;
+    private String length;
+}
