805706cc114cc120aa0f436f02403b562eeb5eef
Stina-Lise <stinalise.hullfors@gmail.com>
2022-02-24 13:40:13 +0100

    Fixat med reviews

--- src/main/java/com/imfdb/IMFDB/controller/MovieController.java
+++ src/main/java/com/imfdb/IMFDB/controller/MovieController.java
@@ -1,18 +1,25 @@
 package com.imfdb.IMFDB.controller;
 
 import com.imfdb.IMFDB.entity.Movie;
-import com.imfdb.IMFDB.repository.MovieRepository;
+import com.imfdb.IMFDB.entity.Review;
+import com.imfdb.IMFDB.repository.ReviewRepository;
 import com.imfdb.IMFDB.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @SuppressWarnings("unused")
 @Controller
 public class MovieController {
+    @Autowired
+    private ReviewRepository reviewRepository;
 
-    MovieService service = new MovieService();
+    @Autowired
+    private MovieService service;
+
+
 
     @GetMapping("/")
     public String getIndex(Model model){
@@ -20,4 +27,20 @@ public class MovieController {
         model.addAttribute("movies", movies);
         return "index";
     }
+
+    @GetMapping("/{id}")
+    public String getMovie(@PathVariable int id, Model model) {
+        Movie movie = service.getMovies().get(0); // ändra detta så att det kopplas till id
+        model.addAttribute("movie", movie);
+        List<Review> reviews = reviewRepository.getReviews();
+        model.addAttribute("reviews", reviews);
+        return "movie";
+    }
+
+    @PostMapping("/reviews")
+    public Review addReview (@RequestBody Review review) {
+        reviewRepository.addReview(review);
+        return review;
+    }
+
 }
---
+++ src/main/java/com/imfdb/IMFDB/entity/Review.java
@@ -0,0 +1,19 @@
+package com.imfdb.IMFDB.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Review {
+    String name;
+    int score;
+    String reviews;
+}
+
+
