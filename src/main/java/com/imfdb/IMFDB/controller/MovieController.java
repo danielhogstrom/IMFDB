78ed8fef2a74d0c09961a978de4e3c7354152d8d
danielhogstrom <daniel.hogstrom@gmail.com>
2022-02-24 13:54:09 +0100

    change MovieController

--- src/main/java/com/imfdb/IMFDB/controller/MovieController.java
+++ src/main/java/com/imfdb/IMFDB/controller/MovieController.java
@@ -1,13 +1,13 @@
 package com.imfdb.IMFDB.controller;
 
 import com.imfdb.IMFDB.entity.Movie;
-import com.imfdb.IMFDB.repository.MovieRepository;
 import com.imfdb.IMFDB.service.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+
 @SuppressWarnings("unused")
 @Controller
 public class MovieController {
@@ -15,7 +15,7 @@ public class MovieController {
     MovieService service = new MovieService();
 
     @GetMapping("/")
-    public String getIndex(Model model){
+    public String getIndex(Model model) {
         List<Movie> movies = service.getMovies();
         model.addAttribute("movies", movies);
         return "index";
