428b43161b12a35e658406f9411709404c990a42
danielhogstrom <daniel.hogstrom@gmail.com>
2022-02-24 08:24:22 +0100

    fix broken img links in MovieRepository

--- src/main/java/com/imfdb/IMFDB/repository/MovieRepository.java
+++ src/main/java/com/imfdb/IMFDB/repository/MovieRepository.java
@@ -17,9 +17,9 @@ public class MovieRepository {
         movies.add(new Movie("Titanic", "Drama", 1997, "James cameron", 11, "https://tinyurl.com/3u63mry4", "3:14"));
         movies.add(new Movie("The Gentleman", "Comedy", 2019, "Guy Ritchie", 15, "https://tinyurl.com/zc282h7u", "1.53"));
         movies.add(new Movie("American Pie", "Comedy", 1999, "Paul Weitz", 15,"https://tinyurl.com/5c3jds4c", "1:40"));
-        movies.add(new Movie("Jurassic Park", "Adventure", 1993, "Steven Spielberg", 11 ,"https://tinyurl.com/2pxa4354", "2:20"));
+        movies.add(new Movie("Jurassic Park", "Adventure", 1993, "Steven Spielberg", 11 ,"https://tinyurl.com/2p8eh9j5", "2:20"));
         movies.add(new Movie("The Big Blue", "Drama", 1987, "Luc Besson", 11, "https://tinyurl.com/2p83wwpc", "2:48"));
-        movies.add(new Movie("The adventures of Tintin", "Drama", 2011, "Steven Spielberg", 11, "https://tinyurl.com/mtjev39r", "1:47"));
+        movies.add(new Movie("The adventures of Tintin", "Drama", 2011, "Steven Spielberg", 11, "https://tinyurl.com/5xamvdfs", "1:47"));
     }
 
     public boolean addMovie(Movie movie){
