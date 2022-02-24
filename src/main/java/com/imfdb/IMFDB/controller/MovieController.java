cbeec4619e7f65a0d58723360121e7508c1d5c43
danielhogstrom <daniel.hogstrom@gmail.com>
2022-02-24 09:40:41 +0100

    add MovieService

--- src/main/java/com/imfdb/IMFDB/controller/MovieController.java
+++ src/main/java/com/imfdb/IMFDB/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.imfdb.IMFDB.controller;
 
 import com.imfdb.IMFDB.entity.Movie;
 import com.imfdb.IMFDB.repository.MovieRepository;
+import com.imfdb.IMFDB.service.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,11 @@ import java.util.List;
 @Controller
 public class MovieController {
 
-    MovieRepository repository = new MovieRepository();
+    MovieService service = new MovieService();
 
     @GetMapping("/")
     public String getIndex(Model model){
-        List<Movie> movies = repository.getMovies();
+        List<Movie> movies = service.getMovies();
         model.addAttribute("movies", movies);
         return "index";
     }
---
+++ src/main/java/com/imfdb/IMFDB/service/MovieService.java
@@ -0,0 +1,18 @@
+package com.imfdb.IMFDB.service;
+
+import com.imfdb.IMFDB.entity.Movie;
+import com.imfdb.IMFDB.repository.MovieRepository;
+
+import java.util.List;
+
+public class MovieService {
+    MovieRepository repository;
+
+    public MovieService(){
+        repository = new MovieRepository();
+    }
+
+    public List<Movie> getMovies(){
+        return repository.getMovies();
+    }
+}
