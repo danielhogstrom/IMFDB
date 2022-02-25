d8154b7e1559b6bbcc12cab891c48cf25e61f1be
Stina-Lise <stinalise.hullfors@gmail.com>
2022-02-25 11:12:02 +0100

    Gjort ett test

--- src/test/java/com/imfdb/IMFDB/ImfdbApplicationTests.java
+++ src/test/java/com/imfdb/IMFDB/ImfdbApplicationTests.java
@@ -1,13 +1,25 @@
 package com.imfdb.IMFDB;
 
+import com.imfdb.IMFDB.entity.Movie;
+import com.imfdb.IMFDB.service.MovieService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ImfdbApplicationTests {
+	@Autowired
+MovieService service;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testFindMovie() {
+		Movie movie = service.findMovieById(3);
+		Assertions.assertEquals(3, movie.getId());
 	}
 
 }
