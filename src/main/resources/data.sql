0c3bab4da1edb3f46d155dfe0548f82c51061cd1
TheoPersson <theodeleopersson@gmail.com>
2022-03-03 14:13:49 +0100

    Uppdaterat genres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,44 +1,17 @@
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('The Lord of the rings', 'Fantasy', 'Its like the book about a ring, but more of a movie' ,2001, 'Peter Jackson', 11, 'https://tinyurl.com/4e4pe9fm', '2:58');
+INSERT INTO MOVIES (title, description, year_made, director, recommended_age, img_url, movie_length)
+VALUES ('The Lord of the rings', 'Its like the book about a ring, but more of a movie' ,2001, 'Peter Jackson', 11, 'https://tinyurl.com/4e4pe9fm', '2:58');
 
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Titanic', 'Drama', 'Leonardo paints like she is a french girl' ,1997, 'James cameron', 11, 'https://tinyurl.com/3u63mry4', '3:14');
+INSERT INTO GENRE (genre)
+VALUES('Drama');
 
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('The Gentleman', 'Comedy', 'LA story about not so gentle men' ,2019, 'Guy Ritchie', 15, 'https://tinyurl.com/guyRitchman', '1:53');
+INSERT INTO GENRE (genre)
+VALUES('Fantasy');
 
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('American Pie', 'Comedy', 'A love story about a guy and his pie' ,1999, 'Paul Weitz', 15, 'https://tinyurl.com/5c3jds4c', '1:40');
+INSERT INTO GENRE_MOVIES (GENRE_ID,MOVIES_ID)
+VALUES (1,1);
 
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Jurassic Park', 'Adventure', 'A movie about a adventure park owner that spares no expense' ,1993, 'Steven Spielberg', 11, 'https://tinyurl.com/2p8eh9j5', '2:20');
+INSERT INTO GENRE_MOVIES (GENRE_ID,MOVIES_ID)
+VALUES (2,1);
 
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('The Big Blue', 'Drama', 'Its about a lake or something idk..' ,1987, 'Luc Besson', 11, 'https://tinyurl.com/2p83wwpc', '2:48');
 
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('The Adventures of Tintin', 'Drama', 'Your beloved french cartoon comes to life with the direction of Speilbergo' ,2011, 'Steven Spielberg', 11, 'https://tinyurl.com/5xamvdfs', '1:47');
 
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Resident Evil: Welcome to Raccoon City', 'Horror', 'A movie that hits you like a pile of steaming garbage' ,2021, 'Johannes Roberts', 15, 'https://tinyurl.com/residenteviiiil', '1:47');
-
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Twins', 'Comedy', 'Funny movie about one long guy and one short' ,1988, 'Ivan Reitman', 11, 'https://tinyurl.com/twinstwin', '1:47');
-
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Lethal Weapon', 'Action', 'Mr.Gibson shoots a lot of bad guys and Mr.Glover is to old for this sh*t' ,1987, 'Richard Donner', 11, 'https://tinyurl.com/leathalWEaponnnn', '1:49');
-
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Uncharted', 'Adventure', 'Wahlburger is funny and Tom takes of his shirt' ,2022, 'Ruben Fleischer', 11, 'https://tinyurl.com/unchartedd', '1:56');
-
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('The Bourne Identity', 'Action', 'Matt Damon is Jason Bourne but remembers.... NOTHING!' ,2002, 'Doug Liman', 11, 'https://tinyurl.com/bourneidentt', '1:59');
-
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Puss In Boots', 'Adventure', 'Now Zorro is a cat in boots' ,2011, 'Chris Miller', 8, 'https://tinyurl.com/pussinbootyz', '1:30');
-
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Snakes on a Plane', 'Adventure', 'Samuel l. Jackson fights a bunch of snakes on a plane' ,2006, 'David R. Ellis', 15, 'https://tinyurl.com/snakesonplaness', '1:45');
-
-INSERT INTO MOVIES (title, genre, description, year_made, director, recommended_age, img_url, movie_length)
-VALUES ('Finding Nemo', 'Adventure', 'A small piece of sushi is lost at sea' ,2003, 'Andrew Stanton', 8, 'https://tinyurl.com/findingNemoz', '1:40');
